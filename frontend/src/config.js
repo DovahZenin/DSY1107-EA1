fe3b901fdc09ae59938727a2ef5474135f0729c8
JOOORMUNDGANDER <168875426+DovahZenin@users.noreply.github.com>
2026-09-10 16:38:04 -0300

    Revert "feat: Integración de backend Spring Boot con RDS y correcciones de CORS/OAuth2"
    This reverts commit 89a5f06551d2c591c0b029988729e1f4ffe1b8a5.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export const CONFIG = {
-  cognitoDomain: "https://dsy1107-grupo007-app.auth.us-east-1.amazoncognito.com", 
-  clientId: "20740plil1mrctf04rtvmdpgep",
+  cognitoDomain: "https://dsy1107-grupo007.auth.us-east-1.amazoncognito.com", 
+  clientId: "3kd0fp4hbo8358uck751tm4eh6",
   redirectUri: "http://localhost:5173/",
-  apiGatewayUrl: "http://localhost:8080" // Cambiar a tu backend local
+  apiGatewayUrl: "https://sr29maec79.execute-api.us-east-1.amazonaws.com"
 };
